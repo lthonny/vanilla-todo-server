8cd1c32c698c3cbbf31087f3da30acd967bffb77
vasilich <thonny02@mail.ru>
2021-07-11 22:08:40 +0300

    create json file

--- server.js
+++ server.js
@@ -1,22 +1,15 @@
 const express = require('express');
-const path = require('path')
+const path = require('path');
+const cors = require('cors')
 const app = express();
 
 const port = 3333;
 
-// app.get('/', (req, res) => {
-//   res.send("Hello, rest api");
-//   res.sendFile(path.join(__dirname, '/tasks.json'))
-// });
-
-// app.use(express.static('index.html'));
-
-app.get('/', function (req, res) {
-  res.sendFile(path.normalize(__dirname + '/tasks.json'))
+app.get('/', cors(), function (req, res) {
+    res.sendFile(path.normalize(__dirname + '/tasks.json'));
 })
 
 
-
 app.listen(port, () => {
-  console.log(`Server is up ${port}`);
+    console.log(`Server is up ${port}`);
 });
