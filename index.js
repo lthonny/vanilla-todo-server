17011d95e232279a7ff17b91b83b4e84db633ab1
vasiliy <thonny02@maail.ru>
2021-07-12 17:52:56 +0300

    method rest api

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const todoRoutes = require('./routes/routes.js');
 
-const todoRoutes = require('./routes/todo');
 
+app.use(cors());
 app.use(todoRoutes);
 
 
-const PORT = process.env.PORT || 3333;
+
+
+const PORT = process.env.PORT ?? 3333;
+
 app.listen(PORT, () => {
     console.log(`Server is up ${PORT}`);
 });
